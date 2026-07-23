8990ccd4b15399d4aa12a5a978f7b51f8b4d4a98
Cejera <132576561+cejera@users.noreply.github.com>
2026-07-23 18:29:40 -0300

    Fix: Remove SetStaticDefaults() - use .hjson localization file instead

--- Buffs/RessonanciaC1.cs
+++ Buffs/RessonanciaC1.cs
@@ -5,10 +5,5 @@ namespace ROTMod.Buffs
 {
     public class RessonanciaC1 : ModBuff
     {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.AddTranslation("en-US", "Ressonância C1");
-            Description.AddTranslation("en-US", "Você está em sintonia com o ROT!\nNenhum custo de vida para usar suas habilidades.");
-        }
     }
 }
